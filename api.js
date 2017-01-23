3d365dea4e072dd0dd9dafbaba5a50cbaa5fbbaf
Mike Purgatori <mpurgatori@gmail.com>
2017-01-22 23:08:08 -0600

    More pi fixin

--- api.js
+++ api.js
@@ -2,11 +2,8 @@ var express = require('express');
 const router = express.Router();
 
 
-var userRoute = require('./routes/userRoute');
 var weatherRoute = require('./routes/weatherRoute');
 
-
-router.use('/user', userRoute);
 router.use('/weather', weatherRoute);
 
 
