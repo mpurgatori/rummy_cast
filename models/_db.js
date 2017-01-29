ea9c18ac805a9a04fbd22b374b39b635cae0c01b
Mike Purgatori <mpurgatori@gmail.com>
2017-01-29 17:07:08 -0600

    Udpate to front end, chrome

--- models/_db.js
+++
@@ -1,7 +0,0 @@
-let Sequelize = require('sequelize');
-
-let db = new Sequelize('postgres://localhost:5432/Teach', {
-  logging: false
-});
-
-module.exports = db;
--- public/customJS.js
+++ public/customJS.js
@@ -1,93 +1,113 @@
 
-
-
-
-function nameFunc(){
+function nameFunc() {
+  console.log('SOMETHING')
   if (document.cookie.slice(0,4) === 'name'){
-      $("#nameInput").hide();
-      $("#loadName").text(document.cookie.slice(5));
+      $('#nameInput').hide();
+      $('#loadName').text(document.cookie.slice(5));
       }
   else{
-      $(".enter_link").click(function (e) {
-        e.preventDefault();
-      $("#loadName").text(($("#inputName").val()));
+      console.log('SOMETHING ELSE');
+      $('.enter_link').click(function (e) {
+      e.preventDefault();
+      $('#loadName').text(($('#inputName').val()));
       var username = document.form1.firstName.value;
-      document.cookie = "name=" + username;
-      $(this).parent("#nameInput").fadeOut(500);
-    })
+      document.cookie = 'name=' + username;
+      $(this).parent('#nameInput').fadeOut(500);
+    });
   }
 }
 
 function reSubmit(){
-  $(".enter_link").click(function (e) {
+  $('.enter_link').click(function (e) {
     e.preventDefault();
-  $("#loadName").text(($("#inputName").val()));
+  $('#loadName').text(($('#inputName').val()));
   var username = document.form1.firstName.value;
-  document.cookie = "name=" + username;
-  $(this).parent("#nameInput").fadeOut(500);
-})
+  document.cookie = 'name=' + username;
+  $(this).parent('#nameInput').fadeOut(500);
+});
 }
 
+// function backgrounds(){
+//   $(function () {
+//       var body = $('body');
+//       var backgrounds = [
+//         'url(http://static.jsbin.com/images/jsbin_static.png)',
+//         'url(http://static.jsbin.com/images/popout.png)'];
+//       var current = 0;
+//
+//       function nextBackground() {
+//           body.css(
+//               'background',
+//           backgrounds[current = ++current % backgrounds.length]);
+//
+//           setTimeout(nextBackground, 5000);
+//       }
+//       setTimeout(nextBackground, 5000);
+//       body.css('background', backgrounds[0]);
+//   });
+// }
+
+
 function clearName(){
-  $(".change_name").click(function(e){
+  $('.change_name').click(function(e){
       e.preventDefault();
-      $("#nameInput").fadeIn(300);
+      $('#nameInput').fadeIn(300);
       reSubmit();
   })
 }
 
 //Clock
 function updateClock ( ) {
- 	let currentTime = new Date ();
-  	let currentHours = currentTime.getHours ();
-  	let currentMinutes = currentTime.getMinutes ();
-  	//let currentSeconds = currentTime.getSeconds ( );
+ 	var currentTime = new Date ();
+  	var currentHours = currentTime.getHours ();
+  	var currentMinutes = currentTime.getMinutes ();
+  	//var currentSeconds = currentTime.getSeconds ( );
 
   	// Pad the minutes and seconds with leading zeros, if required
-  	currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-  	//currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+  	currentMinutes = ( currentMinutes < 10 ? '0' : '' ) + currentMinutes;
+  	//currentSeconds = ( currentSeconds < 10 ? '0' : '' ) + currentSeconds;
 
-  	// Choose either "AM" or "PM" as appropriate
-  	let timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+  	// Choose either 'AM' or 'PM' as appropriate
+  	var timeOfDay = ( currentHours < 12 ) ? 'AM' : 'PM';
 
   	// Convert the hours component to 12-hour format if needed
   	currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
 
-  	// Convert an hours component of "0" to "12"
+  	// Convert an hours component of '0' to '12'
   	currentHours = ( currentHours == 0 ) ? 12 : currentHours;
 
   	// Compose the string for display
-  	let currentTimeString = currentHours + ":" + currentMinutes +" "+timeOfDay;
+  	var currentTimeString = currentHours + ':' + currentMinutes +' '+timeOfDay;
 
 
-   	$("#clock").html(currentTimeString);
+   	$('#clock').html(currentTimeString);
 
  }
 
 function greeting(){
-  let currentTime = new Date ();
-  let currentHours = currentTime.getHours ();
-  let greet;
+  var currentTime = new Date ();
+  var currentHours = currentTime.getHours ();
+  var greet;
   if (currentHours<12){
-    greet="Good Morning";
+    greet='Good Morning';
   }
-  if (currentHours >=12 && currentHours<17){
-    greet="Good Afternoon";
+  else if (currentHours >=12 && currentHours<17){
+    greet='Good Afternoon';
   }
-  else{
-    greet="Good Evening";
+  else if (currentHours >= 17){
+    greet='Good Evening';
   }
-  $( "#loadGreet" ).text( greet );
+  $( '#loadGreet' ).text( greet );
 }
 
 
  function getWeather(){
-   var str = "00B0";
+   var str = '00B0';
    var degreeSign = String.fromCharCode(parseInt(str, 16));
-   $.get( "/api/weather", function( data ) {
-     var fahrenheit = Math.round(data.temperature)+degreeSign+' F';
-    $( "i" ).addClass( data.icon );
-    $( "#temp" ).text( fahrenheit );
+   $.get( '/api/weather', function( data ) {
+     var fahrenheit = Math.round(data.temperature)+degreeSign+' F in Chicago';
+    $( 'i' ).addClass( data.icon );
+    $( '#temp' ).text( fahrenheit );
     });
  }
 
